9613125121e95dc14961012170774888649ebcdf
Mohammed shafi <shafiking6922@gmail.com>
2024-01-17 19:49:22 +0530

    Design:Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#4F6F52',
+        'secondry':'#739072',
+        'light-primary':'#86A789',
+        'light-secondry':'#D2E3C8',
+      },
+    },
   },
   plugins: [],
 }
